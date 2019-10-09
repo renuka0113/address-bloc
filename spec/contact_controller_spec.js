864bec371af16be126e4c6424923e2434823a346
Renuka <renukaalurkar@gmail.com>
2019-10-09 12:19:45 -0500

    contact_controller_spec

--- spec/contact_controller_spec.js
+++ spec/contact_controller_spec.js
@@ -2,8 +2,27 @@ const ContactController = require("../controllers/ContactController");
 const sequelize = require("../db/models/index").sequelize;
 describe("ContactController", () => {
 
-  it("should be defined", () => {
-    expect(ContactController).toBeDefined();
-  });
+  beforeEach((done)=>{
+    this.book=new ContactController();
 
-});
+    // #1
+
+    sequelize.sync({force:true}).then((res)=>{
+      done();
+    }) //sequelize close
+
+.catch((err)=>{
+  done();
+  }); //catch close
+}); //beforeEach close
+
+//#2
+ describe("#addContact()",()=>{
+   it("should add a single contact into the book",()=>{
+     expect(this.books.contact.length).toBe(0);
+     this.book.addContact("Alice","001-101-1010");
+     expect(this.books.contacts.length).toBe(1);
+   });//it close
+
+ });//addContact close
+})//ContactController close
